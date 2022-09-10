86126e447284e5eef2b89c4faa78d86686752e68
ruifernandees <dev.ruifernandes@gmail.com>
2022-09-10 20:38:47 -0300

    FEAT: remove multiple comments (incomplete)

--- src/analyzer/lexical/utils/remove-comments.cpp
+++ src/analyzer/lexical/utils/remove-comments.cpp
@@ -12,15 +12,44 @@ using namespace std;
 vector<string> removeComments(vector<string> text)
 {
     vector<string> cmds;
+    bool hasBeginCommentStatement = false;
     for (string line : text)
     {
         string newLine;
         for (int i = 0; i < line.length(); i++)
         {
+            if (line[i] == '=') {
+                cout << "IGUAL CHAR" << endl;
+                string beginString = "";
+                string endString = "";
+                for (int k = i; k < MULTIPLE_LINE_COMMENT_BEGIN.size(); k++) {
+                    if (k >= line.length()) break;
+                    beginString.push_back(line[k]);
+                }
+                for (int l = i; l < MULTIPLE_LINE_COMMENT_END.size(); l++) {
+                    if (l >= line.length()) break;
+                    endString.push_back(line[l]);
+                }
+                if (beginString.compare(MULTIPLE_LINE_COMMENT_BEGIN) == 0) {
+                    cout << "BEGIN STRING" << endl;
+                    hasBeginCommentStatement = true;
+                    // i += MULTIPLE_LINE_COMMENT_BEGIN.size() - 1;
+                }
+                if (endString.compare(MULTIPLE_LINE_COMMENT_END) == 0) {
+                    cout << "END STRING" << endl;
+                    hasBeginCommentStatement = false;
+                    // i += MULTIPLE_LINE_COMMENT_END.size() - 1;
+                }
+            }
+            if (hasBeginCommentStatement) break;
             if (line[i] == ONE_LINE_COMMENT) break;
             newLine.push_back(line[i]);
         }
+        cout << newLine << endl;
         cmds.push_back(newLine);
+    }
+    if (hasBeginCommentStatement) {
+        throw runtime_error("Error: unclosed begin comment statement");
     }
     return cmds;
 }
--- src/bnf/types.h
+++ src/bnf/types.h
@@ -1,3 +1,6 @@
+#include <string>
+#include <cstring>
+
 #define KEYWORD "KEYWORD"
 #define IDENTIFIER "IDENTIFIER"
 #define OPERATOR "OPERATOR"
@@ -6,9 +9,14 @@
 
 #define ONE_LINE_COMMENT '#'
 
+
+
 #pragma once
 
 using namespace std;
+
+const string MULTIPLE_LINE_COMMENT_BEGIN = "=begin";
+const string MULTIPLE_LINE_COMMENT_END = "=end";
 
 typedef struct aux {
   string content;
