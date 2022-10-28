f0889e6de8bc29c2c5d665a8d4c9fd639f2f5474
JonathanYuri <jonathanyuri2323@gmail.com>
2022-10-28 19:17:06 -0300

    FIX: eat

--- src/analyzer/syntactic/utils/eat.cpp
+++ src/analyzer/syntactic/utils/eat.cpp
@@ -1,13 +1,5 @@
 #pragma once
 
-bool verify_content(vector<Token> tokens, int *currentToken, string content)
-{
-    if (tokens[*currentToken].content.compare(content) == 0) {
-        return eat(tokens, currentToken);
-    }
-    return false;
-}
-
 bool eat(vector<Token> tokens, int *currentToken)
 {
     if (*currentToken + 1 >= tokens.size()) {
@@ -16,4 +8,12 @@ bool eat(vector<Token> tokens, int *currentToken)
     }
     *currentToken = *currentToken + 1;
     return true;
+}
+
+bool verify_content(vector<Token> tokens, int *currentToken, string content)
+{
+    if (tokens[*currentToken].content.compare(content) == 0) {
+        return eat(tokens, currentToken);
+    }
+    return false;
 }
