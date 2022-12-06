50cf7cc1fcc0835de326c56a02cacb0165d1e7ad
heldaolima <orfeowrk@gmail.com>
2022-12-06 17:26:03 -0300

    deixai toda esperança ó vós que entrais

--- src/analyzer/syntactic/syntactic-analyzer.cpp
+++ src/analyzer/syntactic/syntactic-analyzer.cpp
@@ -18,6 +18,7 @@ typedef struct scope {
 
 typedef struct var_scope {
     string name;
+    int value;
     Scope scope;
 } var_scope;
 
@@ -55,8 +56,69 @@ void scope_type(vector<Token> tokens, vector<Scope> keywords, stack<Scope> *esco
     }
 }
 
-vector<var_scope> tabela_de_simbolos(vector<Token> tokens)
+// vector<var_scope> tabela_de_simbolos(vector<Token> tokens)
+// {
+//     vector<var_scope> symbols_table;
+
+//     vector<Scope> block_keys = {{"def", "function"}, {"class", "class"}, {"if", "if"}, {"elsif", "elsif"}, {"else", "else"}, {"for", "for"}, {"while", "while"}, 
+//     {"begin", "try"}, };
+    
+//     stack<Scope> escopos;
+    
+//     Scope padrao = newScope("padrao", "padrao");
+
+//     for (int i = 0; i < tokens.size(); i++) {
+//         scope_type(tokens, block_keys, &escopos, &i);
+
+//         if (tokens[i].type.compare(IDENTIFIER) == 0) {
+//             if (escopos.size() == 0) {
+//                 symbols_table.push_back({tokens[i].content, {"padrao", "padrao"}});
+//             } 
+//             else {
+//                 symbols_table.push_back({tokens[i].content, {escopos.top().name, escopos.top().type}});
+//             }
+//         }
+        
+//         if (tokens[i].content.compare("end") == 0) {
+//             escopos.pop();
+//         }
+
+//     }
+
+//     return symbols_table;
+
+// }
+
+int search_var(vector<var_scope> symbols_table, string var_name, string scope_name) {
+    int i = 0;
+    for (i = 0; i < symbols_table.size(); i++) {
+        if (symbols_table[i].name.compare(var_name) == 0 && symbols_table[i].scope.name.compare(scope_name) == 0) 
+            return i;
+    }
+
+    return -1;
+}
+
+int somar();
+int subtrair();
+int multiplicar();
+int dividir();
+
+void atribuir(vector<var_scope>* symbols_table, vector<Token> tokens, int* pos_tokens, int pos_var)
 {
+    /*
+    while (tokens[*pos_tokens].type.compare(DELIMITER) != 0) {
+        // ideia: escolher a funcao adequada segundo o operador. o problema está nas operações mais complexas
+        // outra ideia: já que é uma atribuição, atualizar o valor da variavel em pos_var a cada num/identificador lido
+        // assim voce pode realizar operacoes com a propria variavel principal pq é só ir acumulando
+        *pos_tokens++;
+    }
+    */
+}
+
+vector<var_scope> tabela_de_simbolos1111(vector<Token> tokens)
+{
+    int pos = 0;
     vector<var_scope> symbols_table;
 
     vector<Scope> block_keys = {{"def", "function"}, {"class", "class"}, {"if", "if"}, {"elsif", "elsif"}, {"else", "else"}, {"for", "for"}, {"while", "while"}, 
@@ -66,16 +128,45 @@ vector<var_scope> tabela_de_simbolos(vector<Token> tokens)
     
     Scope padrao = newScope("padrao", "padrao");
 
+
     for (int i = 0; i < tokens.size(); i++) {
         scope_type(tokens, block_keys, &escopos, &i);
 
         if (tokens[i].type.compare(IDENTIFIER) == 0) {
+            
+            
             if (escopos.size() == 0) {
-                symbols_table.push_back({tokens[i].content, {"padrao", "padrao"}});
+                pos = search_var(symbols_table, tokens[i].content, "padrao");
+                if (pos == -1) {
+                    symbols_table.push_back({tokens[i].content, 0, {"padrao", "padrao"}});
+                }
+                ++i;
+                if (tokens[i].content.compare("=") == 0) {
+                    ++i;
+                    atribuir(&symbols_table, tokens, &i, pos);
+                }
+                
             } 
             else {
-                symbols_table.push_back({tokens[i].content, {escopos.top().name, escopos.top().type}});
+                pos = search_var(symbols_table, tokens[i].content, escopos.top().name);
+                if (pos == -1)
+                    symbols_table.push_back({tokens[i].content, 0, {escopos.top().name, escopos.top().type}});
             }
+            cout << "\nPOS ao final desta iteração: " << pos << "\n";
+            
+            
+            // if (pos == -1) 
+            //     symbols_table.push_back({tokens[i].name}, 0, "padrao");
+            // else {
+                
+            // }
+
+
+            // while (tokens[i].type.compare(DELIMITER) != 0) {
+            // }
+            
+            
+            
         }
         
         if (tokens[i].content.compare("end") == 0) {
@@ -102,10 +193,10 @@ void syntacticAnalyzer(vector<Token> tokens) {
     cout << "DEU CERTO? " << sem_erros << endl;
     cout << "CurrentToken: " << currentToken << "\n";
     if (sem_erros) {
-        vector<var_scope> tabela = tabela_de_simbolos(tokens);
+        vector<var_scope> tabela = tabela_de_simbolos1111(tokens);
         cout << "TABELA DE SÍMBOLOS: \n";
         for (auto entry: tabela) {
-            cout << entry.name << " | " << entry.scope.name  << " [" << entry.scope.type << "]\n";
+            cout << entry.name << "=" << entry.value << " | " << entry.scope.name  << " [" << entry.scope.type << "]\n";
         }
     }
 }
--- src/mocks/hello-world.rb
+++ src/mocks/hello-world.rb
@@ -1,13 +1,3 @@
-def funcao(asd, ase)
-  return zx
-end
-
 x = 1
 x = x + 1
 
-class myClass
-  var_class = 34
-  def funcao_class()
-    return var_class
-  end
-end
